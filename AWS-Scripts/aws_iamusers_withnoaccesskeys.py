18995c1f74be67bd57078de21639a9f3b9eff3f3
Dam a Tech Guy <104665781+damatechguy27@users.noreply.github.com>
2024-01-31 12:40:15 -0500

    Update aws_iamusers_withnoaccesskeys.py
    updated and better script

--- AWS-Scripts/aws_iamusers_withnoaccesskeys.py
+++ AWS-Scripts/aws_iamusers_withnoaccesskeys.py
@@ -5,26 +5,27 @@ import csv
 from csv import DictWriter
 from datetime import datetime, date
 import os
+from pathlib import Path 
 
 def create_reports_dir():
     today_date = datetime.now().strftime('%Y-%m-%d')
     directory_name = f"reports_{today_date}"
-    script_directory = os.path.dirname(os.path.realpath(__file__))
-    new_directory_path = os.path.join(script_directory, directory_name)
+    directory_path = Path(r'path your want file to be created')
+    full_directory_path = directory_path / directory_name 
 
-    if not os.path.exists(new_directory_path):
-        os.makedirs(new_directory_path)
-        print(f"Directory '{directory_name}' created at: {new_directory_path}")
+    if not full_directory_path.exists():
+        full_directory_path.mkdir(parents=True, exist_ok=True)
+        print(f"Directory '{directory_name}' created at: {directory_path}")
     else:
-        print(f"Directory '{directory_name}' already exists at: {new_directory_path}")
+        print(f"Directory '{directory_name}' already exists at: {directory_path}")
 
-    return directory_name
+    return full_directory_path
 
 def filecheck(file):
     if os.path.exists(file):
         os.remove(file)
 
-def write_to_csv(csvname, users, profile):
+def write_to_csv(csvname, users, profile, iam):
     header = ['Profile', 'Username']
     file_exists = os.path.exists(csvname) and os.path.getsize(csvname) > 0
 
@@ -36,22 +37,27 @@ def write_to_csv(csvname, users, profile):
         for user in users:
             username = user['UserName']
             account_profile = profile
-            writer.writerow({'Profile': account_profile, 'Username': username})
+
+            # Check if the user has access keys
+            access_keys = iam.list_access_keys(UserName=username)['AccessKeyMetadata']
+
+            if not access_keys:
+                writer.writerow({'Profile': account_profile, 'Username': username})
 
 
 def main():
     config = ConfigParser()
-    config.read('Path to aws creds file')
+    config.read('path to cred file')
 
     new_dir = create_reports_dir()
     file_name = f"iam_users_with_noaccess_keys_{date.today()}.csv"
-    file_path = os.path.join(new_dir, file_name)
+    file_path = new_dir / file_name
     filecheck(file_path)
 
     for profile in config.sections():
         session = Session(profile=profile)
         credentials = session.get_credentials()
-        region = 'us-west-2'
+        region = 'region here'
         sesh = boto3.Session(
             aws_access_key_id=credentials.access_key,
             aws_secret_access_key=credentials.secret_key,
@@ -63,7 +69,7 @@ def main():
         iam = sesh.client('iam')
         response = iam.list_users()
         users_without_keys = [user for user in response['Users'] if not user.get('AccessKeys')]
-        write_to_csv(file_path, users_without_keys, profile)
+        write_to_csv(file_path, users_without_keys, profile, iam)
 
     print(f"Data written to '{file_path}'")
 
